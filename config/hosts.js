2a6755afc12c7defea4659240d51a6b75a5f4ad6
Boeny <boeniareadmirr@gmail.com>
2017-05-02 16:42:39 +0300

    msg conflicts fix

--- config/hosts.js
+++ config/hosts.js
@@ -6,9 +6,5 @@
  * <name>/<file> SERVER_ROOT/<HOSTS_DIR>/<name>/<file>.js
  */
 module.exports = {
-<<<<<<< HEAD
 	80: '../craft'
-=======
-	8080: 'p'
->>>>>>> 69c9b394183bd72b0ea729b8f5b09661aedd11e8
 };
